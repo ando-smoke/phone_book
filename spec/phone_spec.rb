92d7323142b555e57652b03f95e90be283001905
Andy Uppendahl <a.uppendahl@gmail.com>
2015-01-16 12:20:10 -0800

    Add 'number' method functionality to Phone class definition.

--- spec/phone_spec.rb
+++ spec/phone_spec.rb
@@ -6,4 +6,9 @@ describe(Phone) do
     test_phone = Phone.new({type: "home", number: "503-555-4444"})
     expect(test_phone.type()).to(eq("home"))
   end
+
+  it("returns the actual number of the phone number") do
+    test_phone = Phone.new({type: "home", number: "503-555-4444"})
+    expect(test_phone.number()).to(eq("503-555-4444"))
+  end
 end
