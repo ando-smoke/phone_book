9cec488716a956c53cc5246a52c38b040c0fdb8f
Andy Uppendahl <a.uppendahl@gmail.com>
2015-01-16 12:40:38 -0800

    Add distinct 'describe' methods for #type and #number Phone class instance methods.

--- spec/phone_spec.rb
+++ spec/phone_spec.rb
@@ -2,13 +2,17 @@ require("rspec")
 require("phone")
 
 describe(Phone) do
-  it("returns the phone type of the phone number") do
-    test_phone = Phone.new({type: "home", number: "503-555-4444"})
-    expect(test_phone.type()).to(eq("home"))
+  describe("#type") do
+    it("returns the phone type of the phone number") do
+      test_phone = Phone.new({type: "home", number: "503-555-4444"})
+      expect(test_phone.type()).to(eq("home"))
+    end
   end
 
-  it("returns the actual number of the phone number") do
-    test_phone = Phone.new({type: "home", number: "503-555-4444"})
-    expect(test_phone.number()).to(eq("503-555-4444"))
+  describe("#number") do
+    it("returns the actual number of the phone number") do
+      test_phone = Phone.new({type: "home", number: "503-555-4444"})
+      expect(test_phone.number()).to(eq("503-555-4444"))
+    end
   end
 end
