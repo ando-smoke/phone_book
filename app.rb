8b3e7edf4f75d2b8309e69bf0057cc95219e612f
Andy Uppendahl <a.uppendahl@gmail.com>
2015-01-16 16:16:28 -0800

    Add initial content/functionality for main index page.

---
+++ app.rb
@@ -0,0 +1,23 @@
+require("sinatra")
+require("sinatra/reloader")
+also_reload("lib/**/*.rb")
+require("./lib/phone")
+require("./lib/contact")
+
+get("/") do
+  @contacts = Contact.all()
+  erb(:index)
+end
+
+post("/add_contact") do
+  first_name = params.fetch("first_name")
+  last_name = params.fetch("last_name")
+  phone_type = params.fetch("phone_type")
+  phone_number = params.fetch("phone_number")
+  phone = Phone.new({ type: phone_type, number: phone_number })
+  new_contact = Contact.new({ first_name: first_name,
+    last_name: last_name, phone_number: [phone] })
+  new_contact.save()
+  @contacts = Contact.all()
+  erb(:index)
+end
